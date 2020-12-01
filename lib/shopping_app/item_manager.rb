4db4c6964f6e334817939c41e123c6f27462a754
Yohei Goto <yohei_goto@diveintocode.jp>
2020-12-01 18:11:29 +0900

    :monkey: Item関連のコメント

--- lib/shopping_app/item_manager.rb
+++ lib/shopping_app/item_manager.rb
@@ -1,19 +1,20 @@
 require "kosi"
 require_relative "item"
 
+# このモジュールをインクルードすると、自身の所有するItemインスタンスを操れるようになります。
 module ItemManager
-  def items
+  def items # 自身の所有する（自身がオーナーとなっている）全てのItemインスタンスを返します。
     Item.all.select{|item| item.owner == self }
   end
 
-  def pick_items(number, quantity)
+  def pick_items(number, quantity) # numberと対応した自身の所有するItemインスタンスを指定されたquantitiy分返します。
     items = stock.find{|stock| stock[:number] == number }&.dig(:items)
     return if items.nil? || items.size < quantity
     items.slice(0, quantity)
   end
 
-  def items_list
-    kosi = Kosi::Table.new({header: %w{番号 商品名 金額 数量}})
+  def items_list # 自身の所有するItemインスタンスの在庫状況を、["番号", "商品名", "金額", "数量"]という列でテーブル形式にして出力します。
+    kosi = Kosi::Table.new({header: %w{番号 商品名 金額 数量}}) # Gemgileに"kosi"のURLを記載
     print kosi.render(
       stock.map do |stock|
         [
@@ -28,9 +29,9 @@ module ItemManager
 
   private
 
-  def stock
+  def stock # 自身の所有するItemインスタンスの在庫状況を返します。
     items
-      .group_by{|item| item.label }
+      .group_by{|item| item.label } # Item#labelで同じ値を返すItemインスタンスで分類します。
       .map.with_index do |label_and_items, index|
         {
           number: index,
@@ -38,7 +39,7 @@ module ItemManager
             name: label_and_items[0][:name],
             price: label_and_items[0][:price],
           },
-          items: label_and_items[1],
+          items: label_and_items[1], # このitemsの中に、分類されたItemインスタンスを格納します。
         }
       end
   end
